1555aa53c5431917e01cebbbc929ff1ff9c92334
shivansh singh <shivanshjobboy@gmail.com>
2022-05-13 02:26:08 +0530

    deploying

--- public/javascript/clusterMap.js
+++ public/javascript/clusterMap.js
@@ -2,7 +2,7 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'cluster-map',
     style: 'mapbox://styles/mapbox/light-v10',
-    center: [ 28.38, 77.12, ],
+    center: [  77.12, 28.38, ],
     zoom: 3
 });
 
