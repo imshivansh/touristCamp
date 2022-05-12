7f0c2eb6ca673d4589c544e4c49ff932fa5302a1
shivansh singh <shivanshjobboy@gmail.com>
2022-05-12 18:57:02 +0530

    deploying

--- dist/index.dev.js
+++ dist/index.dev.js
@@ -55,7 +55,7 @@ var MongoDBStore = require('connect-mongo'); // const options = {
 //   };
 
 
-var dbUrl = process.env.databaseURL || 'mongodb://localhost:27017/yelp-camp';
+var dbUrl = process.env.databaseURL || 'mongodb://0.0.0.0:27017/yelp-camp';
 ;
 mongoose.connect(dbUrl, {
   useNewUrlParser: true,
